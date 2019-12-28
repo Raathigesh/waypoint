20e8ab945a7b60eca686d75891bb0ac342914cdb
Raathigesh <Raathigesh92@gmail.com>
2019-12-28 19:59:26 +1100

    Remove sample project

--- sample-project/query/items.js
+++
@@ -1,2 +0,0 @@
-export const getAllItems = state => state.issues;
-export const getCount = state => state.count;
--- sample-project/query/summart.js
+++
@@ -1,2 +0,0 @@
-export const getSummary = state => state.summary;
-export const getFullText = state => state.text;
