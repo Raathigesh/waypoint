449e9d7278af7eab824d0937bc77edf84d6f3be8
Raathigesh <Raathigesh92@gmail.com>
2020-08-16 19:53:14 +1000

    Fix nightly build

--- scripts/prepare-nightly-build.js
+++ scripts/prepare-nightly-build.js
@@ -1,31 +1,30 @@
 const packageJson = require('../package.json');
 const fs = require('fs');
 const path = require('path');
+var git = require('git-last-commit');
 
-const nextVersion = Number(packageJson.version.split('.')[1]) + 1;
+git.getLastCommit(function(err, commit) {
+    const newPackageJson = {
+        ...packageJson,
+        version: `${packageJson.version}-${commit.shortHash}`,
+        name: 'waypoint-nightly',
+        displayName: 'waypoint nightly',
+    };
 
-const newPackageJson = {
-    ...packageJson,
-    version: `${packageJson.version.split('.')[0]}.${nextVersion}.${
-        packageJson.version.split('.')[2]
-    }`,
-    name: 'waypoint nightly',
-    displayName: 'waypoint nightly',
-};
+    fs.writeFileSync(
+        path.resolve(__dirname, '../package.json'),
+        JSON.stringify(newPackageJson, null, ' ')
+    );
 
-fs.writeFileSync(
-    path.resolve(__dirname, '../package.json'),
-    JSON.stringify(newPackageJson, null, ' ')
-);
+    const readmeContent = fs.readFileSync(
+        path.resolve(__dirname, '../README.md'),
+        'utf8'
+    );
 
-const readmeContent = fs.readFileSync(
-    path.resolve(__dirname, '../README.md'),
-    'utf8'
-);
+    const lines = [
+        '## This is a nightly build of Waypoint. Please use the stable version for day to day use.',
+        ...readmeContent.split(/\r?\n/),
+    ];
 
-const lines = [
-    '## This is a nightly build of Waypoint. Please use the stable version for day to day use.',
-    ...readmeContent.split(/\r?\n/),
-];
-
-fs.writeFileSync(path.resolve(__dirname, '../README.md'), lines.join('\n'));
+    fs.writeFileSync(path.resolve(__dirname, '../README.md'), lines.join('\n'));
+});
